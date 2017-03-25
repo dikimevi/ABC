9f5de629a398f474b435d74e210f2318a34bd18e
dikimevi <dikimevipersonel@gmail.com>
2017-03-25 11:57:38 +0200

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi world!");
+            Console.WriteLine("Hi worldsss!");
         }
     }
 }
